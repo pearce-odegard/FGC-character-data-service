5625999cb2af86c1566eaf61a2a12d3abeadbbe2
podegard89 <pearceodegard@gmail.com>
2023-07-03 18:16:27 -0500

    Scraped data for individual TNS marvel weekly, added README, created character list

---
+++ README.md
@@ -0,0 +1,4 @@
+# Collecting Data about character diversity in Fighting game tournaments
+
+For now, I will only be focusing on characters who place Top 8 at tournaments, and will have a primary focus
+on Ultimate Marvel vs Capcom 3 and Street Fighter 6.
--- bot.js
+++ bot.js
@@ -4,9 +4,8 @@ dotenv.config();
 import puppeteer from "puppeteer-extra";
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 import AdBlockerPlugin from 'puppeteer-extra-plugin-adblocker';
+import { characters } from './charactersUMVC3.js';
 puppeteer.use(StealthPlugin());
-
-import { characters } from './charactersUMVC3';
 
 (async () => {
 
@@ -25,12 +24,41 @@ import { characters } from './charactersUMVC3';
     await waitThenClick('tp-yt-paper-button#expand');
 
     const matchups = await page.$$eval('span.yt-core-attributed-string--link-inherit-color', matchups => {
-        return matchups.map(matchup => matchup.textContent);
+        return matchups.map(matchup => matchup.textContent.slice(0, -2));
     })
 
     const filteredMatchups = matchups.filter((element, index) => {
         return element.includes('vs') && index < 20 || element.includes('vs.') && index < 20;
     });
+
+    // At this point we have successfully scraped data from a single youtube page and acquired raw text that
+    // includes the characters used in the Top 8
+
+    // join the array (removing any commas and parentheses) and search for names?
+    console.log(filteredMatchups.join("").toString())
+    const wordArray = filteredMatchups.join("").toString().replace(/(\(|\)|\,|\.|\\n)/gm, '').split(" ");
+    // console.log(wordArray);
+
+    const charactersInTop8 = [];
+
+    for (const word of wordArray) {
+        if (characters.includes(word) && !charactersInTop8.includes(word)) {
+            charactersInTop8.push(word);
+        }
+    }
+
+    console.log(charactersInTop8);
+
+    // example object for storing data about a given TNS UMvC3 weekly
+    const currentEvent = {
+        title: await page.$eval('a[spellcheck=false]', title => title.textContent),
+        // date: await page.$eval('#info', date => date.textContent),
+        charactersUsed: charactersInTop8
+    }
+
+    console.log(currentEvent);
+
+    // We now have a list of each character that was used in this particular event's Top 8. Nice!
 
     // await browser.close();
 })();
